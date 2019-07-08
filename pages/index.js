d04db1abf0cbb5e2a57a13aa4a904609f2cd68bf
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-08 10:20:21 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import fetch from "isomorphic-fetch";
+import Error from "next/error";
+
+class Index extends React.Component {
+  static async getInitialProps() {
+    let stories;
+    try {
+      const res = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+      stories = await res.json();
+    } catch (err) {
+      console.error(err);
+      stories = [];
+    }
+    return { stories };
+  }
+  render() {
+    const { stories } = this.props;
+    if (stories.length === 0) return <Error statusCode={503} />;
+    return (
+      <div>
+        <h1>hacker next</h1>
+        <div>
+          {stories.map(story => (
+            <h2 key={story.id}>{story.title}</h2>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
