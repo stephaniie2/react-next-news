4ed96d3cd315d408b9a387aca6bfd329a66fcc8a
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-08 11:45:29 +0200

    StoryPage added

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,52 @@
 import fetch from "isomorphic-fetch";
 import Error from "next/error";
-import StoryList from '../components/StoryList';
-import Layout from '../components/Layout';
+import Link from "next/link";
+import StoryList from "../components/StoryList";
+import Layout from "../components/Layout";
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories;
+    let page;
     try {
-      const res = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+      page = Number(query.page) || 1;
+      const res = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`
+      );
       stories = await res.json();
     } catch (err) {
       console.error(err);
       stories = [];
     }
-    return { stories };
+    return { stories, page };
   }
   render() {
-    const { stories } = this.props;
+    const { stories, page } = this.props;
     if (stories.length === 0) return <Error statusCode={503} />;
     return (
       <Layout title="Hacker Next" description="A Hacker Next clone">
         <StoryList stories={stories} />
+        <footer>
+          {page === 1 ? null : (
+            <Link href={`/?page=${page - 1}`}>
+              <a>Prev Page / </a>
+            </Link> 
+          )}
+
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page</a>
+          </Link>
+        </footer>
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
       </Layout>
     );
   }
